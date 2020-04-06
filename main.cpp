a7d084ae13e138ff631fc41932b1d068df7a88aa
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-04-05 22:04:37 -0500

    Terminado?

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 int main() {
-    long long unsigned int n, p, k;
+    int n;
+    double p, k;
 
-    while ( cin>> n >> p) {
+    while ( cin>> n >> p ) {
         k = pow(p, 1.0/ n);
         cout << round(k) << endl;
     }
     return 0;
+
 }
