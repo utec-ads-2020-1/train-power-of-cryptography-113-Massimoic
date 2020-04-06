8a8994fe7176abf05cfaa827f5667904ee8207b5
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-04-05 22:00:54 -0500

    Update avance

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <math.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long long unsigned int n, p, k;
+
+    while ( cin>> n >> p) {
+        k = pow(p, 1.0/ n);
+        cout << round(k) << endl;
+    }
     return 0;
 }
